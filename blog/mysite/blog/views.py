14c425d499bb053468c5d4e9b195dc911ae75399
alex <thesunlight17@gmail.com>
2024-10-30 13:09:01 +0500

    first commit

---
+++ blog/mysite/blog/views.py
@@ -0,0 +1,20 @@
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, "blog/post/list.html", {"posts": posts})
+
+
+def post_detail(request, id):
+    # try:
+    #     post = Post.published.get(id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No such post found.")
+
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+
+    return render(request, "blog/post/detail.html", {"post": post})
